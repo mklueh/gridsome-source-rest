898a4388cdca4fac0922aa8ab3fe9238507b6c01
mklueh90 <marianklueh@gmail.com>
2020-09-21 00:25:55 +0200

    postinstall.js removed

--- postinstall.js
+++
@@ -1,3 +0,0 @@
-const chalk = require('chalk');
-console.log(chalk.green('For more information of the gridsome-plugin-recommender visit'));
-console.log(chalk.red(chalk.underline('https://www.overflowed.dev')));
